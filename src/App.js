f25f92d0351b1184aa8949df2d4e7b558687eabf
PeterHUistyping <2447276617@qq.com>
2023-02-16 21:17:07 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Route,Routes} from "react-router-dom"
+import {Route,Routes } from "react-router-dom"
 import {Home} from "./js/Home"
 import {Blog_Sorting} from "./js/Blog_Sorting"
 import {NotFound} from "./js/404"
--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import { HashRouter as Router} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  
     <Router> 
         <App />
     </Router>
-  </React.StrictMode>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
