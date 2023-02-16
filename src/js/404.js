19806a880934fe6f4a71f6157ca6ead91f0e1df3
PeterHUistyping <2447276617@qq.com>
2023-02-16 19:36:32 +0000

    404

--- src/js/404.js
+++ src/js/404.js
@@ -2,10 +2,13 @@ export function NotFound()
 {
     return(
         <>
+        <Index_Top/>      
         <center>
         Sorry the webpage you want to reach has been moved or doesn't exist.
         <a href="https://peterhuistyping.github.io">🏠HOME</a>
         </center>
+        <Index_Footer />
         </>
+        
     )
 }
