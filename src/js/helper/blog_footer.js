5dd23db6bc554453501939225a131415d7ec5865
PeterHUistyping <2447276617@qq.com>
2023-04-09 23:19:52 +0100

    Add info

--- src/js/helper/blog_footer.js
+++ src/js/helper/blog_footer.js
@@ -36,7 +36,7 @@ function popup() {
     Originally written in Markdown(.md) \n \
     Convert Markdown to HTML via Showdown \n \
     https://showdownjs.com/ \n  \n \
-     ")
+    Please press the button below to Exit. Thank you! ⬇️")
 }
 
  
