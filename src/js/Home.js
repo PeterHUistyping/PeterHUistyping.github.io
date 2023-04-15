16d474428019a681f2996a2e7c2cb6130dbb266b
PeterHUistyping <2447276617@qq.com>
2023-04-15 23:18:05 +0100

    SKILLS

--- src/js/Home.js
+++ src/js/Home.js
@@ -14,13 +14,29 @@ export function Home() {
         Computer Science and how it can be applied in nearly all fields of life. I am open to new fields of area and
         enjoy exploring unknown features. The joy of improving myself step by step is what I enjoy.
     </div>
-
+    
+    <h2><b>TECHNICAL SKILLS</b></h2>
+    Basic Skill <br/>
+    &nbsp;&nbsp;&nbsp;&nbsp;  ·C &nbsp;&nbsp;&nbsp;&nbsp;  ·C++ &nbsp;&nbsp;&nbsp;&nbsp;  ·Java &nbsp;&nbsp;&nbsp;&nbsp;  ·OOP &nbsp;&nbsp;&nbsp;&nbsp;  ·CMake &nbsp;&nbsp;&nbsp;&nbsp;  ·Gdb &nbsp;&nbsp;&nbsp;&nbsp;  ·Algorithms and Data Structure &nbsp;&nbsp;&nbsp;&nbsp;  ·Functional Programming (OCaml)  <br />
+    Front-End <br/> 
+    &nbsp;&nbsp;&nbsp;&nbsp;  ·HTML/ CSS &nbsp;&nbsp;&nbsp;&nbsp;  ·React &nbsp;&nbsp;&nbsp;&nbsp;  ·Latex Back-End &nbsp;&nbsp;&nbsp;&nbsp;  ·Database &nbsp;&nbsp;&nbsp;&nbsp;  ·SQL &nbsp;&nbsp;&nbsp;&nbsp;  ·NoSQL <br />
+    Data Sci <br/>
+    &nbsp;&nbsp;&nbsp;&nbsp;  ·Python &nbsp;&nbsp;&nbsp;&nbsp;  ·NumPy &nbsp;&nbsp;&nbsp;&nbsp;  ·Machine Learning &nbsp;&nbsp;&nbsp;&nbsp;  ·Deep Learning &nbsp;&nbsp;&nbsp;&nbsp;  ·Natural Language Processing &nbsp;&nbsp;&nbsp;&nbsp;  ·Computer Vision <br />
+    Graphics <br/> 
+    &nbsp;&nbsp;&nbsp;&nbsp;  ·Vulkan &nbsp;&nbsp;&nbsp;&nbsp;  ·Graphics Pipeline &nbsp;&nbsp;&nbsp;&nbsp;  ·GPU driver &nbsp;&nbsp;&nbsp;&nbsp;  ·Game Engines/ Unreal Engine Unity &nbsp;&nbsp;&nbsp;&nbsp;  ·C#<br />
+    Math <br/> 
+    &nbsp;&nbsp;&nbsp;&nbsp;  ·Calculus &nbsp;&nbsp;&nbsp;&nbsp;  ·Linear Algebra &nbsp;&nbsp;&nbsp;&nbsp;  ·Discrete Math &nbsp;&nbsp;&nbsp;&nbsp;  ·Probability and Statistics &nbsp;&nbsp;&nbsp;&nbsp;  ·Abstract Algebra &nbsp;&nbsp;&nbsp;&nbsp;  ·Topology (basic) <br />
+    Operating System <br/> 
+    &nbsp;&nbsp;&nbsp;&nbsp;  ·Process Scheduling, Memory (Segment, Page, VM), I/O, File, Linux &nbsp;&nbsp;&nbsp;&nbsp;  ·bash/shell &nbsp;&nbsp;&nbsp;&nbsp;  ·Digital Electronic <br />
+    Development Tools <br/> 
+    &nbsp;&nbsp;&nbsp;&nbsp;  ·git (Version Control), CI/CD pipeline, Docker, Visual Studio, VS Code, Pycharm, Github/Gitlab, IntelliJ IDEA.<br />
+ 
     <h2><b>WORK EXPERIENCE</b></h2>
     
     <b>GPU Software Engineer Intern </b><i> 2022.12- Present</i> <br />
     <img width="48" height="48" src="./asset/photo/Logo/Huawei.png" alt="Huawei"/> 
     Huawei Technologies Research & Development (UK) Ltd 		Cambridge, England, United Kingdom<br />
-    Skills: C++ · CMake · Vulkan · GPU 3D Pipeline · GPU driver · Game Engines/ Unreal Engine ·  Gdb<br />
+    Skills: C++ &nbsp;&nbsp;&nbsp;&nbsp;  · CMake &nbsp;&nbsp;&nbsp;&nbsp;  · Vulkan &nbsp;&nbsp;&nbsp;&nbsp;  · GPU 3D Pipeline &nbsp;&nbsp;&nbsp;&nbsp;  · GPU driver &nbsp;&nbsp;&nbsp;&nbsp;  · Game Engines/ Unreal Engine &nbsp;&nbsp;&nbsp;&nbsp;  ·  Gdb<br />
     
     <h2><b>EDUCATION BACKGROUND</b></h2>
     <b>University of Cambridge </b><i> 2022.10 - Present</i><br />
@@ -40,14 +56,7 @@ export function Home() {
     See more on <a href="https://www.linkedin.com/in/peterhu2022/"> LinkedIn</a><br />
     
     
-    <h2><b>TECHNICAL SKILLS</b></h2>
-    ·C  ·C++ ·Java  ·OOP ·HTML/ CSS  ·CMake  ·Gdb  ·bash/shell  ·git  ·Algorithms and Data Structure <br />
-    ·Database ·SQL  ·NoSQL  ·Latex  ·Functional Programming (OCaml) ·Digital Electronic<br />
-    ·Python ·NumPy  ·Machine Learning  ·Deep Learning   ·Natural Language Processing  ·Computer Vision  <br />  
-    ·Computer Graphics  ·Vulkan  ·GPU 3D Pipeline  ·GPU driver  ·Game Engines/ Unreal Engine<br />
-    Development Tools Visual Studio Code, Visual Studio, Pycharm, Github, IntelliJ IDEA<br />
-    ·Calculus  ·Linear Algebra  ·Discrete Math  ·Probability and Statistics  ·Abstract Algebra  ·Topology (basic)
-    <br/>
+   
 
     <h2 id="project"><b>HONORS and AWARDS</b></h2>
     <HrAw_table />
