932d9f4a9a6bb8df175a3144f301d08069e334e9
PeterHUistyping <2447276617@qq.com>
2023-04-17 10:29:07 +0100

    Add sidebar

--- src/asset/blog/Ref/blogRef.js
+++ src/asset/blog/Ref/blogRef.js
@@ -2,18 +2,64 @@ import ScrollToTop from "../../../js/helper/ScrollToTop";
 import Blog_Footer from "../../../js/helper/blog_footer";
 import Blog_Top from "../../../js/helper/blog_top";
 import Resource  from "./resource";
+import ref from "../../../asset/data/Resource.json"
+import camUn from "../../../asset/data/Resource_CamUn.json"
+import { HashLink } from 'react-router-hash-link';
+ 
 export function Blogref(){
     return(
     <ScrollToTop> 
     <div>
         <Blog_Top/>
             <>
-            <Resource />
-                
+            <p id="resource"><strong><font size="+2"> Useful Resource and Reference for Computer Science📚</font></strong></p>
+            <div class="sidebar">
+                <HashLink to="/ref/#ref">Reference</HashLink>  
+                <HashLink to="/ref/#camUn">Cambridge Undergraduate</HashLink> 
+
+
+            </div>
+            <div class="content">
+                <Resource id="ref"   resource={ref}/>   
+                <br/><br/><br/>
+                <h2 id="camUn">Cambridge Undergraduate</h2>
+                <Resource  resource={camUn}/>   
+                <p>Disclaimer: <i>All rights belong to their respective owners. I do NOT own any of these contents.</i></p>
+        
+           
+            </div>
+            {/* <table class="table_ref"> 
+            <tbody>
+
+            <tr>
+            <td class="table_refSidebar"  rowspan="2">
+                <div>
+                <HashLink to="/ref/#ref">Reference</HashLink> <br></br>
+                <HashLink to="/ref/#camUn">Cambridge Undergraduate</HashLink> <br></br>
+                <HashLink to="/ref/#ref">Open Course Ware</HashLink> <br></br>
+                </div>
+            </td>
+            </tr>
+            <tr>
+            <td rowspan="2">
+            <div>
+                <Resource id="ref"   resource={ref}/>   
+                <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                <h2 id="camUn">Cambridge Undergraduate</h2>
+                <Resource resource={camUn}/>   
+                <p>Disclaimer: <i>All rights belong to their respective owners. I do NOT own any of these contents.</i></p>
+            </div>
+            </td>      
+            </tr>
+            </tbody>
+           </table> */}
+            
+          
             </>
-    
-        <Blog_Footer />
     </div> 
+    <Blog_Footer />
     </ScrollToTop>
     )
 }
--- src/asset/blog/Ref/resource.js
+++ src/asset/blog/Ref/resource.js
@@ -1,20 +1,18 @@
-import resource from "../../../asset/data/Resource.json"
-export default function Resource(){
-    var len=resource.length;
+export default function resource(props){
+    var len=props.resource.length;
     var keys = [...Array(len).keys()];
     keys.sort((a,b)=>a-b);
     return(
         <>
-        <p id="resource"><strong><font size="+2"> Useful Resource and Reference for Computer Science📚</font></strong></p>
         {keys.map((item, index)=>(
             <>
-            {index+1}. <a href={resource[item].link}>{resource[item].description}</a>
+            {index+1}. <a href={props.resource[item].link}>{props.resource[item].description}</a>
             <br/>
-            Author: <i>{resource[item].author}</i>
+            Author: <i>{props.resource[item].author}</i>
             <br/> <br/>
             </>
         ))}  
-        <p>Disclaimer: <i>All rights belong to their respective owners. I do NOT own any of these contents.</i></p>
+      
         </>
     )
 }
