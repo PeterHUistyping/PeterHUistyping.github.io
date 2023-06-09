f50295b89873f1778af1699b64b15fefdf4a6c88
PeterHUistyping <2447276617@qq.com>
2023-06-09 16:57:24 +0100

    Better format, remove duplicate and redundant cv

--- src/js/Home.js
+++ src/js/Home.js
@@ -9,35 +9,24 @@ export function Home() {
     <div>
     <Index_Top/>       
     <div class="h2_center">About Me</div>
-    <div class="container">
+    <div class="aboutMeContainer">
         I am a Computer Science student studying at University of Cambridge. What I really enjoy is the beauty of
         Computer Science and how it can be applied in nearly all fields of life. I am open to new fields of area and
         enjoy exploring unknown features. The joy of improving myself step by step is what I enjoy.
+        <br/><br/> Exploring areas in Graphics, ML, AI, Math, Algorithms, etc. 
     </div>
-    
+    <center>
+    {/* <embed src="asset/doc/CV_PeterHU.pdf"  width="800px" height="600px" /> */}
+    </center>
     <h2><b>TECHNICAL SKILLS</b></h2>
-    Basic Skill <br/>
-    &nbsp;&nbsp;&nbsp;&nbsp;  ·C &nbsp;&nbsp;&nbsp;&nbsp;  ·C++ &nbsp;&nbsp;&nbsp;&nbsp;  ·Java &nbsp;&nbsp;&nbsp;&nbsp;  ·OOP &nbsp;&nbsp;&nbsp;&nbsp;  ·CMake &nbsp;&nbsp;&nbsp;&nbsp;  ·Gdb &nbsp;&nbsp;&nbsp;&nbsp;  ·Algorithms and Data Structure &nbsp;&nbsp;&nbsp;&nbsp;  ·Functional Programming (OCaml)  <br />
-    Front-End <br/> 
-    &nbsp;&nbsp;&nbsp;&nbsp;  ·HTML/ CSS &nbsp;&nbsp;&nbsp;&nbsp;  ·React &nbsp;&nbsp;&nbsp;&nbsp;  ·Latex Back-End &nbsp;&nbsp;&nbsp;&nbsp;  ·Database &nbsp;&nbsp;&nbsp;&nbsp;  ·SQL &nbsp;&nbsp;&nbsp;&nbsp;  ·NoSQL <br />
-    Data Sci <br/>
-    &nbsp;&nbsp;&nbsp;&nbsp;  ·Python &nbsp;&nbsp;&nbsp;&nbsp;  ·NumPy &nbsp;&nbsp;&nbsp;&nbsp;  ·Machine Learning &nbsp;&nbsp;&nbsp;&nbsp;  ·Deep Learning &nbsp;&nbsp;&nbsp;&nbsp;  ·Natural Language Processing &nbsp;&nbsp;&nbsp;&nbsp;  ·Computer Vision <br />
-    Graphics <br/> 
-    &nbsp;&nbsp;&nbsp;&nbsp;  ·Vulkan &nbsp;&nbsp;&nbsp;&nbsp;  ·Graphics Pipeline &nbsp;&nbsp;&nbsp;&nbsp;  ·GPU driver &nbsp;&nbsp;&nbsp;&nbsp;  ·Game Engines/ Unreal Engine Unity &nbsp;&nbsp;&nbsp;&nbsp;  ·C#<br />
-    Math <br/> 
-    &nbsp;&nbsp;&nbsp;&nbsp;  ·Calculus &nbsp;&nbsp;&nbsp;&nbsp;  ·Linear Algebra &nbsp;&nbsp;&nbsp;&nbsp;  ·Discrete Math &nbsp;&nbsp;&nbsp;&nbsp;  ·Probability and Statistics &nbsp;&nbsp;&nbsp;&nbsp;  ·Abstract Algebra &nbsp;&nbsp;&nbsp;&nbsp;  ·Topology (basic) <br />
-    Operating System <br/> 
-    &nbsp;&nbsp;&nbsp;&nbsp;  ·Process Scheduling, Memory (Segment, Page, VM), I/O, File, Linux &nbsp;&nbsp;&nbsp;&nbsp;  ·bash/shell &nbsp;&nbsp;&nbsp;&nbsp;  ·Digital Electronic <br />
-    Development Tools <br/> 
-    &nbsp;&nbsp;&nbsp;&nbsp;  ·git (Version Control), CI/CD pipeline, Docker, Visual Studio, VS Code, Pycharm, Github/Gitlab, IntelliJ IDEA.<br />
+    <i>Please refer to SKILLS section in  <a href="./asset/doc/CV_PeterHU.pdf">CV</a>.</i>
  
     <h2><b>WORK EXPERIENCE</b></h2>
     
     <b>GPU Software Engineer Intern </b><i> 2022.12- Present</i> <br />
     <img width="48" height="48" src="./asset/photo/Logo/Huawei.png" alt="Huawei"/> 
     Huawei Technologies Research & Development (UK) Ltd 		Cambridge, England, United Kingdom<br />
-    Skills: C++ &nbsp;&nbsp;&nbsp;&nbsp;  · CMake &nbsp;&nbsp;&nbsp;&nbsp;  · Vulkan &nbsp;&nbsp;&nbsp;&nbsp;  · GPU 3D Pipeline &nbsp;&nbsp;&nbsp;&nbsp;  · GPU driver &nbsp;&nbsp;&nbsp;&nbsp;  · Game Engines/ Unreal Engine &nbsp;&nbsp;&nbsp;&nbsp;  ·  Gdb<br />
-    
+    <br/><i>Please refer to EXPERIENCE section in <a href="./asset/doc/CV_PeterHU.pdf">CV</a>.</i>
     <h2><b>EDUCATION BACKGROUND</b></h2>
     <b>University of Cambridge </b><i> 2022.10 - Present</i><br />
     <img width="54" height="48" src="./asset/photo/Logo/Cam.png" alt="Cambridge" /> 
@@ -53,7 +42,8 @@ export function Home() {
     <b>Nanyang Model High School </b> <i> 2018.09 - 2021.06</i><br />
     High School Diploma<br />
     Physics Rep, Leader of a research project. I have been awarded ShuPing Scholarship twice.<br />
-    See more on <a href="https://www.linkedin.com/in/peterhu2022/"> LinkedIn</a><br />
+    <br></br>
+    <i>See more on <a href="https://www.linkedin.com/in/peterhu2022/"> LinkedIn</a></i><br />
     
     
    
