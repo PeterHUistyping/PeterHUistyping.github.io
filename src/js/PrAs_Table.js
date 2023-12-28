92f8f65149622554c8cbfc91abaf7af42baeb1cd
PeterHUistyping <2447276617@qq.com>
2023-12-28 17:28:23 +0000

    Update see more

--- src/js/PrAs_Table.js
+++ src/js/PrAs_Table.js
@@ -8,30 +8,29 @@ import Visual_Computing from '../asset/data/Visual_Computing.json';
 export default function PrAs_table(){
    return (
        <div>
-          <p id="Visual_Computing"><b> <font size="+1"><center>Visual Computing</center></font></b></p> 
+     <p id="Visual_Computing"><b> <font size="+1"><center>Visual Computing</center></font></b></p> 
+       <All_table resource={Visual_Computing}/> 
        <center>
+          See more → &nbsp;
             <a href="https://peterhuistyping.github.io/Visual_Computing">🌐 Projects</a>   |   
             <a href="https://github.com/PeterHUistyping/Visual_Computing">👨‍💻 Github</a>  <br></br>
        </center>
-       
-       <All_table resource={Visual_Computing}/> 
 
        <p id="Machine_Learning"><b><font size="+1"><center>Machine Learning</center></font></b></p>
+       <All_table resource={Machine_Learning}/> 
        <center>
+          See more → &nbsp;
             <a href="https://peterhuistyping.github.io/Machine_Learning_Guidance">🌐 Projects</a>  |  
             <a href="https://github.com/PeterHUistyping/Machine_Learning_Guidance">👨‍💻 Github</a>   <br></br>
-       
        </center>
-       <All_table resource={Machine_Learning}/> 
 
-
-       <p id="Computer_System"><b> <font size="+1"><center>Computer System Design</center></font></b></p> 
+       <p id="Computer_System"><b> <font size="+1"><center>Computer System Design</center></font></b></p>        
+       <All_table resource={Computer_System}/> 
        <center>
+          See more → &nbsp;
             <a href="https://peterhuistyping.github.io/Computer_System_Guidance">🌐 Projects</a>   |   
             <a href="https://github.com/PeterHUistyping/Computer_System_Guidance">👨‍💻 Github</a>  <br></br>
        </center>
-       
-       <All_table resource={Computer_System}/> 
-       
+
        </div>)
 }
