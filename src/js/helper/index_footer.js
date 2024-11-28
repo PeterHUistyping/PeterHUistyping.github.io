d7818aefb625c23bf5aab029184f92d93038456c
PeterHUistyping <2447276617@qq.com>
2024-11-28 16:34:16 +0000

    footer remove contact

--- src/js/helper/index_footer.js
+++ src/js/helper/index_footer.js
@@ -5,20 +5,21 @@ import FooterCommon from "./footerCommom"
 export default function Index_Footer(){
     return(
     <>
+    <br/>
     <hr width="50%" color="#987cb9" SIZE="1" /> 
     <table class="table_footer">
     <tbody>
         <td >
-            <strong>Contact Details </strong>
+            {/* <strong>Contact Details </strong>
             <img width= "12vw" src="/asset/photo/Logo/e-mail.png" alt="email"/><a href="mailto:zh369@cam.ac.uk" >Email</a>  &nbsp; 
              <img width= "12vw" src="/asset/photo/Logo/phone.png" alt="phone"/>
-            <a href="tel:4407990719841">Tel</a> | 
-             {/* (+44) 07990 719841 */}
-             <a href="https://github.com/PeterHUistyping"> <img width= "15vw" src="/asset/photo/Logo/github.svg" alt="github"/></a> |  
+            <a href="tel:4407990719841">Tel</a> |  */}
+                {/* (+44) 07990 719841 */}
+             {/* <a href="https://github.com/PeterHUistyping"> <img width= "15vw" src="/asset/photo/Logo/github.svg" alt="github"/></a> |   */}
              {/* <a href="https://www.linkedin.com/in/peterhu2022/"> <img width= "16vw" src="/asset/photo/Logo/linkedin.svg" alt="linkedin"/></a> | */}
-             <a href="https://www.kaggle.com/peterhu2022"> <img width= "16vw" src="/asset/photo/Logo/kaggle.svg" alt="linkedin"/></a> 
-              {/* | More*/ }
-              <br/> 
+             {/* <a href="https://www.kaggle.com/peterhu2022"> <img width= "16vw" src="/asset/photo/Logo/kaggle.svg" alt="linkedin"/></a>  */}
+                {/* | More*/ }
+              {/* <br/>  */}
 
             <FooterCommon />   
             </td>
