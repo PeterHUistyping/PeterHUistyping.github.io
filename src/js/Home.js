9702740924461af54eb8c0ed022c047965e4e634
PeterHUistyping <2447276617@qq.com>
2024-11-28 16:16:15 +0000

    icon update with links

--- src/js/Home.js
+++ src/js/Home.js
@@ -24,21 +24,26 @@ function Education(){
     <center>
     <large><i>EDUCATION</i></large>
     </center>
+    <a href="https://www.cam.ac.uk/">
     <img width="27" height="24" src="./asset/photo/Logo/Cam.png" alt="Cambridge" /> 
+    </a>
     <b> University of Cambridge </b><i> <br />
         Undergraduate<br />
         2022.10 - 2025.06</i><br />
         Computer Science | <a href="https://peterhuistyping.github.io/Cambridge_CS_Tripos/">Notes</a><br />
         [Fully-funded] <a href="https://en.wikipedia.org/wiki/Jardine_Scholarship">Jardine</a> <a href="./asset/doc/Jardine_Certificate.jpg">Scholarship</a>. <br />
-
+    <a href="https://en.xmu.edu.cn/main.htm">
     <img width="24" height="24" src="./asset/photo/Logo/xmu.png" alt="XMU"/>
+    </a>
     <b> Xiamen University </b><i> <br />
         First year undergraduate<br /> 
         2021.09 - 2022.06</i> <br />
         Software Engineering<br />
         GPA: 3.91 / 4.0 (first term)<br /> 88 / 100 (yearly)<br />
         Class Rep, ACM Team.<br />
-
+    <a href="https://en.wikipedia.org/wiki/Shanghai_Nanyang_Model_High_School">
+    <img width="28" height="24" src="./asset/photo/Logo/NanyangHigh.png" alt="Nanyang"/>
+    </a>
     <b>Nanyang Model High School </b> <br />
         <i> 2018.09 - 2021.06</i><br />
         {/* High School Diploma<br /> */}
