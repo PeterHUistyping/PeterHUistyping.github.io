cc0f652c8a12e2ea53eb220d69be65db65772a15
PeterHUistyping <2447276617@qq.com>
2025-04-13 01:12:18 +0100

    ResearchDescriptionButton; Journey Behind the Research

--- src/js/Research.js
+++ src/js/Research.js
@@ -20,18 +20,101 @@ export function Publication(){
 
 }
 
+
+export function ResearchDescriptionButton(props){
+    // usage: <ResearchDescriptionButton 
+    //         title = {<>HI!</>}  mainText = {<>Hi</>} />
+
+    const [show , setShow] = useState(false);
+  
+    const handleOnClick = () => {
+          setShow(! show);
+    }
+
+    return(
+        <>
+        <button  
+        style={{backgroundColor: show ? "#f1f8e9": "white",
+            border: "1px solid #4CAF50",
+            width: "80%",
+            marginLeft: "10%",
+            borderTopLeftRadius: "12px",
+            borderTopRightRadius: "12px",
+            marginTop: "0",     
+            marginBottom: "0", 
+            padding: "5px",   
+        }} onClick={handleOnClick}
+        // on mouse over change border width to 2px
+        onMouseOver={(e) => e.currentTarget.style.border = "2px solid #4CAF50"}
+        onMouseOut={(e) => e.currentTarget.style.border = "1px solid #4CAF50"}
+        >   
+            <div>
+                <p>{props.title} &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; {show ? <>△</>:<>▼</>}</p>
+            </div>
+        </button>
+
+        {show && 
+        <div style={{width: "74%", margin: "auto"}}>
+        {props.mainText}
+
+        {/* add horizontal dashed green line */}
+        <div style={{borderBottom: "1px dashed #4CAF50", width: "100%", marginTop: "10px"}}></div>
+        </div>}
+
+
+        </>
+    )
+}
+
+
+export function ResearchStory(){
+    return(
+        <>
+        <h3 id="researchStory"><b>Journey Behind the Research</b></h3>
+
+        <ResearchDescriptionButton 
+            title={<><b>ABOUT my first paper</b> <i>NeuMaDiff: Neural Material Synthesis via Hyperdiffusion.</i></>}
+
+            mainText={ <>
+            Coauthored with my supervisors <a href="https://chenliang-zhou.github.io/">Chenliang Zhou</a> and Dr <a href="https://asztr.github.io/">Alejandro Sztrajman</a>, we together propose different aspects to the novel architecture, where I draw the network architecture via LaTeX <a href="https://github.com/pgf-tikz/pgf">TikZ</a>. Our methods support the unconditional, statistically constrained and multi-modal generation of materials. With my experiments on PCA, VAE taking in either original or neural materials, we found out that hyperdiffusion has superiority in generating materials with high fidelity and diversity. <br/>
+
+            Team up with Dr <a href="https://asztr.github.io/">Alejandro Sztrajman</a>, we further explore and finalize the statistical constraints for each type of material, e.g. plastic, fabric, metallic, mirror-like, etc, based on observation and experiments. It serves as a classical, as well as ML-free way to classify the material. <br/>
+        
+            In particular, there is a lack of effective BRDF-space metrics in generation tasks, where we fill the gap through my experiments, targeting the diversity and fidelity with various underlying distance functions tried. Through literature review, I raise the idea of adapting point cloud based metrics to the field of materials. With further discussions with <a href="https://chenliang-zhou.github.io/">Chenliang Zhou</a>, we finalize on which pairwise distance measurements to use. To further illustrate its effectiveness, I utilize the confusion matrix to demonstrate the evaluation process. With help of my plotted graph, it's clear that our methods are able to identify the relationship between reference and synthetic material sets. <br/>
+        
+            Finally, as a proof of concept, I setup some typical scenes using the proposed materials and render them with the physically-based renderer <a href="http://www.mitsuba-renderer.org/">Mitsuba3</a>, which I writeup the BSDF class taking in the required binary format with proper importance sampling strategies (since there's no such public error-free implementation for Python version Mitsuba3).<br/> 
+        </>}
+        ></ResearchDescriptionButton>
+        <br/>  
+
+        <ResearchDescriptionButton 
+            title = {<><b>ABOUT my collaborative </b> <i>CHOrD: Generation of Collision-Free, ..., Digital Twins for 3D Indoor Scenes.</i></>}
+            mainText = {<>
+              Mentored by Dr <a href="https://www.cl.cam.ac.uk/~fz261/">Fangcheng Zhong</a>, we investigated how 2D floor plans generation can be integrated with 3D indoor scene synthesis, for lower collision rate and higher fidelity. 
+              In addition, a new real-world indoor scenes dataset will be released.
+              <br/>
+
+              I am responsible for reproducing the prior art <a href="https://tangjiapeng.github.io/projects/DiffuScene/">DiffuScene, CVPR24</a> and <a href="https://chenguolin.github.io/projects/InstructScene/"> InstructScene, ICLR24 Spotlight</a>. Meanwhile, I investigated the YOLO-v8 Oriented Bounding Box (OBB) <a href="https://github.com/ultralytics/ultralytics/issues/15805">detection error</a> and proposed alternative methods. 
+              <br/>
+
+              The former <a href="https://tangjiapeng.github.io/projects/DiffuScene/">DiffuScene</a> is based on applying diffusion model for parametrized objects (location, size, orientation, class, shape code). For greater instruction control, the latter <a href="https://chenguolin.github.io/projects/InstructScene/"> InstructScene</a> follows a two-stage procedure. A semantic graph, with class label, (additional) pairwise spatial relationship and quantized features, is constructed from the prior semantic input. After that, a separate 3D decoder determines the exact layout (location, size, orientation).
+              <br/>
+
+            </>}
+        ></ResearchDescriptionButton>
+        <br/>  
+
+
+        </>
+    )
+}
+
+
 export function Research(){
     useEffect(() => {
             document.title = 'Research - Peter HU';
     }, []);
 
-    const [show , setShow] = useState(false);
-    const [show2 , setShow2] = useState(false);
-    // const [show3 , setShow3] = useState(false);
-  
-    const handleOnClick = () => {
-          setShow(! show);
-    }
     return (
     <motion.div> 
     <Index_Top/> 
@@ -50,7 +133,7 @@ export function Research(){
         I am privileged to undertake some interesting academic research explorations @<a href="https://core-lab.io/">Core Lab</a> at the intersection of Computer Graphics, Computer Vision and Machine Learning. During this period, I explore the neural representations of materials, physical accuracy, generative models with evaluation, real-world material synthesis, and many other. 
         <br/>     
         My undergraduate research is supervised by <a href="https://chenliang-zhou.github.io/">Chenliang Zhou</a> on generative machine learning and Dr <a href="https://asztr.github.io/">Alejandro Sztrajman</a> on real-world materials and <a href="https://en.wikipedia.org/wiki/Bidirectional_reflectance_distribution_function">Bidirectional Reflectance Distribution Function (BRDF)</a>; mentored by
-        Dr <a href="https://www.cl.cam.ac.uk/~fz261/">Fangcheng Zhou</a> on Graphics (geometry representations and rendering), indoor layout synthesis.
+        Dr <a href="https://www.cl.cam.ac.uk/~fz261/">Fangcheng Zhong</a> on Graphics (geometry representations and rendering), indoor layout synthesis.
         <br/>
         I am honored to be the recipient of <a href="asset/doc/HuResearch20250327.pdf">2025 Cambridge Summer Internship and Research Award</a>.
         <br/>
@@ -58,43 +141,16 @@ export function Research(){
         <br/> <br/>
         
         <br/> 
-        <h2 id="pub"><b>List of Publications</b></h2>
+        <h3 id="pub"><b>List of Publications</b></h3>
         <Publication/>
 
         <br/>
 
-        <button  
-        style={{backgroundColor: show ? "#f1f8e9": "white",
-            border: "1px solid #4CAF50",
-            width: "80%",
-            marginLeft: "10%",
-            borderTopLeftRadius: "12px",
-            borderTopRightRadius: "12px",
-        }} onClick={handleOnClick}
-        // on mouse over change border width to 2px
-        onMouseOver={(e) => e.currentTarget.style.border = "2px solid #4CAF50"}
-        onMouseOut={(e) => e.currentTarget.style.border = "1px solid #4CAF50"}
-        >   
-            <div>
-                <p><b>ABOUT my first paper</b> <i>NeuMaDiff: Neural Material Synthesis via Hyperdiffusion</i> ... &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                {show ? <>△</>:<>▼</>}</p>
-            </div>
-        </button>
-
-        {show && 
-        <div style={{width: "74%", margin: "auto"}}>
-        Coauthored with my supervisors <a href="https://chenliang-zhou.github.io/">Chenliang Zhou</a> and Dr <a href="https://asztr.github.io/">Alejandro Sztrajman</a>, we together propose different aspects to the novel architecture, where I draw the network architecture via LaTeX <a href="https://github.com/pgf-tikz/pgf">TikZ</a>. Our methods support the unconditional, statistically constrained and multi-modal generation of materials. With my experiments on PCA, VAE taking in either original or neural materials, we found out that hyperdiffusion has superiority in generating materials with high fidelity and diversity. <br/>
-        Team up with Dr <a href="https://asztr.github.io/">Alejandro Sztrajman</a>, we further explore and finalize the statistical constraints for each type of material, e.g. plastic, fabric, metallic, mirror-like, etc, based on observation and experiments. It serves as a classical, as well as ML-free way to classify the material. <br/>
-        In particular, there is a lack of effective BRDF-space metrics in generation tasks, where we fill the gap through my experiments, targeting the diversity and fidelity with various underlying distance functions tried. Through literature review, I raise the idea of adapting point cloud based metrics to the field of materials. With further discussions with <a href="https://chenliang-zhou.github.io/">Chenliang Zhou</a>, we finalize on which pairwise distance measurements to use. To further illustrate its effectiveness, I utilize the confusion matrix to demonstrate the evaluation process. With help of my plotted graph, it's clear that our methods are able to identify the relationship between reference and synthetic material sets. <br/>
-        Finally, as a proof of concept, I setup some typical scenes using the proposed materials and render them with the physically-based renderer <a href="http://www.mitsuba-renderer.org/">Mitsuba3</a>, which I writeup the BSDF class taking in the required binary format with proper importance sampling strategies (since there's no such public error-free implementation for Python version Mitsuba3).<br/> 
-        </div>}
-
-        <br/>  
-
+        <ResearchStory />
         
         <br/>  
         
-        <h2 id="talk"><b>Talk</b></h2>
+        <h3 id="talk"><b>Talk</b></h3>
         <TalkRayTracing showall={true}/>
         <br/> <br/> 
 
