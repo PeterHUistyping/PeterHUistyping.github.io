40f75099df5a41386e470550fc4dd39a66aef148
PeterHUistyping <2447276617@qq.com>
2026-02-10 22:02:38 +0000

    update reviewer experience

--- src/js/Research.js
+++ src/js/Research.js
@@ -32,8 +32,14 @@ function Review(){
             With thanks to the nomination and invitation, I have served as the <br/>
             <div style={{width: "75%", margin: "10px auto", textAlign: "left"}}>
 
-                <i>Peer Reviewer in the <a href="https://unireps.org/2025/">UniReps Workshop</a>, 39th Conference on Neural Information Processing Systems (NeurIPS'25). </i>
+                <i>Peer Reviewer in the <a href="https://unireps.org/2025/">UniReps Workshop</a>, 39th Conference on Neural Information Processing Systems (NeurIPS'25), <br/>
+                {/* Workshop on Unifying Representations in Neural Systems */}
+                <span style={{paddingLeft: "8.5em"}}>
+                    <a href="https://sites.google.com/view/iclr-2026-workshop-cao/">CAO Workshop</a>, 40th International Conference on Learning Representations (ICLR' 26). <br/>
+                    {/* Catch, Adapt, and Operate: Monitoring ML Models Under Drift */}
+                </span>
                 
+                </i>
             </div>
 
         </div>
